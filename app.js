ee4f0e6a095d1b7c9158a627f88aad5ab5e9dfb3
zekraquraan <zekraquraan7@gmail.com>
2023-03-09 13:12:45 +0000

    he

--- app.js
+++ app.js
@@ -52,31 +52,42 @@ let employee7=new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior");
 //console.log(employee1);
 methodCaller(allEmployees);
 
-// employee1.randomSalary();
-// employee1.netSalary();
-// employee2.randomSalary();
-// employee2.netSalary();
-// employee3.randomSalary();
-// employee3.netSalary();
-// employee4.randomSalary();
-// employee4.netSalary();
-// employee5.randomSalary();
-// employee5.netSalary();
-// employee6.randomSalary();
-// employee6.netSalary();
-// employee7.randomSalary();
-// employee7.netSalary();
-// employee1.render();
-// employee2.render();
-// employee3.render();
-// employee4.render();
-// employee5.render();
-// employee6.render();
-// employee7.render();
 console.log(allEmployees);
+function uniqueId(){
+  Math.floor(Math.random() * 1000)
+  
+}
+allEmployees[this.employeeId].uniqueId();
 function methodCaller(){
 for(let i=0;i<allEmployees.length;i++){
   allEmployees[i].randomSalary();
   allEmployees[i].render();
  
 }}
+let h1El=document.getElementsByTagName("heading");
+console.log(h1El);
+let body=document.getElementsByName("body");
+//console.log(body[0]);
+let h3El=document.createElement('h3');
+console.log(h3El);
+h3El.textContent="welcome";
+//console.log(h3El);
+//body[0].appendChild(h3El);
+let submit=document.getElementById("submit");
+
+
+submit.addEventListener('click', function() {
+  allEmployees[i].Employee();
+});
+let h2El=document.getElementsByTagName("cards");
+console.log(h2El);
+let cardEl=document.getElementByClass("container");
+console.log(cardEl);
+let imgEl=document.createElement('img');
+imgEl.src=this.imageUrl;
+cardEl.appendChild(imgEl);
+let h4El=document.createElement('h4');
+console.log(h4El);
+let h5El=document.createElement('h5');
+console.log(h5El);
+
