8bea99fa2e306dd3e70f9306c8fa250642a52498
zekraquraan <zekraquraan7@gmail.com>
2023-03-11 20:03:07 +0000

    hy

--- app.js
+++ app.js
@@ -9,39 +9,36 @@ function Employee(employeeId,fullName,department,level,imageUrl,salary) {
     this.level=level;
     this.imageUrl=imageUrl;
     this.salary=0;
-    allEmployees.push(this);
-   
+   allEmployees.push(this);
 }
 Employee.prototype.randomSalary=function () {
     console.log(this);
    
     if(this.level=="Junior"){
        
-        this.salary=Math.floor(Math.random() * (1000 - 500) ) + 500;
+        this.salary=randomNumber(500,1000);
         
-
-console.log(this.salary);
     }
     else if(this.level=="Mid-Senior"){
-        this.salary=Math.floor(Math.random() * (1500- 1000) ) + 1000;
-console.log(this.salary);
-    }
-    else if(this.level=="Senior"){
+        this.salary=randomNumber(1000,1500);
         
-        this.salary=Math.floor(Math.random() * (2000- 1500) ) + 1500;
-    
-console.log(this.salary);
     }
+    else if (this.level=="Senior"){
+        
+        this.salary=randomNumber(1500,2000);
+
+    }
+    this.salary=netSalary(this.salary);
   }
-  Employee.prototype.netSalary=function (){
-    
-    (this.salary=this.salary*(1-.075));
-   console.log(this.salary);
-  }
+ 
   Employee.prototype.render=function(){
-document.write(`<h3>employeeName is ${this.fullName}, Salary: ${this.salary}</h3>`);
+document.write(`<h3>employeeName is ${this.fullName},${this.salary}</h3>`);
+const card = document.getElementsByClassName("card");
+    //card.allEmployees = card(allEmployees);
 
   }
+
+
 let employee1 =new Employee(1000,"Ghazi Samer","Administration","Senior");
 let employee2=new Employee(1001,"Lana Ali","Finance","Senior");
 let employee3=new Employee(1002,"Tamara Ayoub","Marketing","Senior");
@@ -49,45 +46,43 @@ let employee4=new Employee(1003,"Safi Walid","Administration","Mid-Senior");
 let employee5=new Employee(1004,"Omar Zaid","Development","	Senior");
 let employee6=new Employee(1005,"Rana Saleh","Development","Junior");
 let employee7=new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior");
-//console.log(employee1);
-methodCaller(allEmployees);
 
 console.log(allEmployees);
-function uniqueId(){
-  Math.floor(Math.random() * 1000)
-  
+methodCaller(allEmployees);
+function randomNumber(min,max) {
+  return Math.floor(Math.random()*(max-min))+min;
 }
-allEmployees[this.employeeId].uniqueId();
-function methodCaller(){
-for(let i=0;i<allEmployees.length;i++){
-  allEmployees[i].randomSalary();
-  allEmployees[i].render();
- 
-}}
-let h1El=document.getElementsByTagName("heading");
-console.log(h1El);
-let body=document.getElementsByName("body");
-//console.log(body[0]);
-let h3El=document.createElement('h3');
-console.log(h3El);
-h3El.textContent="welcome";
-//console.log(h3El);
-//body[0].appendChild(h3El);
-let submit=document.getElementById("submit");
-
-
-submit.addEventListener('click', function() {
-  allEmployees[i].Employee();
-});
-let h2El=document.getElementsByTagName("cards");
-console.log(h2El);
-let cardEl=document.getElementByClass("container");
-console.log(cardEl);
-let imgEl=document.createElement('img');
-imgEl.src=this.imageUrl;
-cardEl.appendChild(imgEl);
-let h4El=document.createElement('h4');
-console.log(h4El);
-let h5El=document.createElement('h5');
-console.log(h5El);
-
+function netSalary(salary){
+return salary-(salary * 0.075);
+}
+function methodCaller(allEmployees){
+  for(let i=0;i<allEmployees.length;i++){
+    allEmployees[i].randomSalary();
+    allEmployees[i].render();
+   
+  }}
+  function uniqueId(allEmployees){
+    Math.floor(Math.random() * 10000);
+    this.employeeId=uniqueId(this.employeeId);
+  }
+  function modifyText(new_text) {
+    const fullname = document.getElementById("fullname");
+    fullname.firstChild.nodeValue = new_text;
+    const department = document.getElementById("department");
+    department.secondChild.nodeValue = new_text;
+    const level = document.getElementById("level");
+    level.secondChild.nodeValue = new_text;
+    const imageurl = document.getElementById("imageurl");
+    imageurl.thirdChild.nodeValue = new_text;
+  }
+  
+  
+  const el = document.getElementById("e1");
+  if(e1){
+  el.addEventListener(
+    "click",
+    () => {
+      modifyText("done");
+    },
+    false
+  );}
